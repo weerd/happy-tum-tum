737f8f4f213644d22258ddcae2f4c0004b01ad47
Diego Lorenzo <diego@diegolorenzo.com>
2020-03-16 02:13:03 -0400

    Fixing bug with prefixed assets.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withCSS = require('@zeit/next-css');
 const withPlugins = require('next-compose-plugins');
 
 const nextConfig = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/happy-tum-tum' : '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/happy-tum-tum' : '',
   exportPathMap: function() {
     return {
       '/': { page: '/' },
