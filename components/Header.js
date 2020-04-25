3dd1cf2f189d2d13fb4443123a7f1afabe6f83c1
Diego Lorenzo <diego@diegolorenzo.com>
2020-04-25 17:07:19 -0400

    Updating the header.

--- components/Header.js
+++ components/Header.js
@@ -2,15 +2,26 @@ const Header = props => (
   <>
     <header className='bg-gray-700'>
       <div className='lg:max-w-3xl lg:mx-auto pt-8 px-3 md:px-16'>
-        <h2 className='font-normal mb-2 text-center text-lg text-gray-600 uppercase'>
-          FODMAP Food Reference
-        </h2>
+        <div className='md:flex md:items-center mb-4 md:w-1/2 mx-auto'>
+          <img
+            alt='happy tum tum logo'
+            className='mx-auto md:mx-0'
+            src='/happy_tum_tum.svg'
+            style={{ width: 130 }}
+          />
 
-        <h1 className='font-bold mb-4 text-center text-4xl text-gray-300'>
-          Happy Tum-Tum
-        </h1>
+          <div className='md:ml-4'>
+            <h2 className='font-normal mb-2 text-center text-lg text-gray-600 uppercase'>
+              FODMAP Food Reference
+            </h2>
 
-        <p className='text-gray-100'>
+            <h1 className='font-bold text-center text-4xl text-gray-300'>
+              Happy Tum-Tum
+            </h1>
+          </div>
+        </div>
+
+        <p className='md:w-3/4 mx-auto text-gray-100'>
           Search for food items below and find out whether they are considered
           low or high FODMAP foods that could impact your digestion.
         </p>
