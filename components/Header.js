d8288fcd0928e6324cce0517115d16bb4c74fc5f
Diego Lorenzo <diego@diegolorenzo.com>
2020-04-29 21:16:35 -0400

    Updates to header.

--- components/Header.js
+++ components/Header.js
@@ -1,27 +1,26 @@
-const Header = props => (
+const Header = (props) => (
   <>
     <header className='bg-gray-700'>
-      <div className='lg:max-w-3xl lg:mx-auto pt-8 px-3 md:px-16'>
-        <div className='md:flex md:items-center mb-4 md:w-1/2 mx-auto'>
+      <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-screen-md mx-auto px-4'>
+        {/* lg:max-w-3xl lg:mx-auto pt-8 px-3 md:px-16 */}
+        <div className='md:col-span-1'>
           <img
             alt='happy tum tum logo'
-            className='mx-auto md:mx-0'
+            className='mx-auto'
             src='/happy_tum_tum.svg'
-            style={{ width: 130 }}
+            style={{ width: 110 }}
           />
-
-          <div className='md:ml-4'>
-            <h2 className='font-normal mb-2 text-center text-lg text-gray-600 uppercase'>
-              FODMAP Food Reference
-            </h2>
-
-            <h1 className='font-bold text-center text-4xl text-gray-300'>
-              Happy Tum-Tum
-            </h1>
-          </div>
         </div>
 
-        <p className='md:w-3/4 mx-auto text-gray-100'>
+        <div className='md:col-span-2 text-center md:text-left'>
+          <h2 className='font-normal mb-2 text-lg text-gray-600 uppercase'>
+            FODMAP Food Reference
+          </h2>
+
+          <h1 className='font-bold text-4xl text-gray-300'>Happy Tum-Tum</h1>
+        </div>
+
+        <p className='md:col-span-3 text-gray-100'>
           Search for food items below and find out whether they are considered
           low or high FODMAP foods that could impact your digestion.
         </p>
